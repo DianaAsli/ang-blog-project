be097afdb2b31a21df6ba7fab718a9b1eca16ffb
DianaAsli <dianaasli201001@gmail.com>
2024-12-08 00:34:10 +0200

    Add dotenv configuration to load environment variables

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
-const CONNECTION_STRING = 'mongodb://localhost:27017/ang-blog-app';
+//const CONNECTION_STRING = 'mongodb://localhost:27017/ang-blog-app';
 
 module.exports = async(app) =>{
     try{
-        await mongoose.connect(CONNECTION_STRING);
+        await mongoose.connect(process.env.CONNECTION_STRING);
         console.log('Database connected');
     } catch(err){
         console.error(err.message);
--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const databaseConfig = require('./config/database');
 const routesConfig = require('./config/routes')
 
+require('dotenv').config();
+
 start();
 
 async function start() {
@@ -10,5 +12,5 @@ async function start() {
     await databaseConfig(app);
     routesConfig(app);
 
-    app.listen(3000, ()=>console.log('Server is listening on port 3000'))
+    app.listen(process.env.PORT, ()=>console.log('Server is listening on port 3000'))
 }
