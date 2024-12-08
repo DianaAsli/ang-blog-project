7324326677ffee49a6fe9971790987c994805536
DianaAsli <dianaasli201001@gmail.com>
2024-12-08 14:48:15 +0200

    Test get all users

--- backend/controllers/homeController.js
+++ backend/controllers/homeController.js
@@ -1,7 +1,21 @@
+const { getAllUsers } = require('../services/homeService');
+
 const homeController = require('express').Router();
 
 homeController.get('/', (req,res)=>{
     res.send('API working');
 })
+
+homeController.get('/users', async (req, res,next) => {
+    try {
+        const users = await getAllUsers();
+        if (users.length === 0) {
+            return res.status(404).json({ message: 'No users found' });
+        }
+        res.json(users);
+    } catch (error) {
+        next(error)
+    }
+});
 
 module.exports=homeController;
