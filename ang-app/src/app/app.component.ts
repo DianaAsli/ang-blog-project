3f690fe815d3fa485f0d75516c5cd1891ac11c65
DianaAsli <dianaasli201001@gmail.com>
2024-12-08 23:51:22 +0200

    Add footer component

--- ang-app/src/app/app.component.ts
+++ ang-app/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './layouts/header/header.component';
 import { NavbarComponent } from './layouts/navbar/navbar.component';
-//import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './layouts/footer/footer.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, NavbarComponent],
+  imports: [RouterOutlet, HeaderComponent, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
