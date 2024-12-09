492ba2bb2c97b1faea18afed527c1e062af5abbc
DianaAsli <dianaasli201001@gmail.com>
2024-12-09 12:50:54 +0200

    Create login component

--- ang-app/src/app/app.component.ts
+++ ang-app/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { NavbarComponent } from './layouts/navbar/navbar.component';
 import { FooterComponent } from './layouts/footer/footer.component';
 import { RouterOutlet } from '@angular/router';
 import { RegisterComponent } from "./auth/register/register.component";
+import { LoginComponent } from './auth/login/login.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, NavbarComponent, FooterComponent, RegisterComponent],
+  imports: [RouterOutlet, HeaderComponent, NavbarComponent, FooterComponent, RegisterComponent, LoginComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
