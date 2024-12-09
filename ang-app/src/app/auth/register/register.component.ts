0cca72ffabee403a7d5f1284397e06b63df383bc
DianaAsli <dianaasli201001@gmail.com>
2024-12-09 18:04:15 +0200

    Add Angular form handling with ngModel and ngForm for Register component
    Add validation for required fields with error display in Register form (email field)

--- ang-app/src/app/auth/register/register.component.ts
+++ ang-app/src/app/auth/register/register.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
 
+  formSubmit(form: any) {
+    console.log('Form submitted');
+    console.log(form.value);
+
+  }
 }
