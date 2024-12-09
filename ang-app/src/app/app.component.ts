bf018b80c2a90d9d21a7b82f20dc21ec20c1fea8
DianaAsli <dianaasli201001@gmail.com>
2024-12-09 12:35:05 +0200

    Optimize footer styling

--- ang-app/src/app/app.component.ts
+++ ang-app/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { HeaderComponent } from './layouts/header/header.component';
 import { NavbarComponent } from './layouts/navbar/navbar.component';
 import { FooterComponent } from './layouts/footer/footer.component';
 import { RouterOutlet } from '@angular/router';
+import { RegisterComponent } from "./auth/register/register.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, NavbarComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, NavbarComponent, FooterComponent, RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
