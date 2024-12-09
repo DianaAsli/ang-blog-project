35ea7510eb60102effc0268cb1749b5aeee95e04
DianaAsli <dianaasli201001@gmail.com>
2024-12-09 23:21:28 +0200

    Add form validation and error messages for registration fields

--- ang-app/src/app/auth/register/register.component.ts
+++ ang-app/src/app/auth/register/register.component.ts
@@ -10,7 +10,30 @@ import { FormsModule, NgForm } from '@angular/forms';
 })
 export class RegisterComponent {
 
+
+  getErrorMessage(input: any, fieldName: String): String | null{
+      if(input.errors.required){
+        return `${fieldName} is required.`;
+      } else if(input.errors?.email){
+        return 'Email is invalid.';
+      } else if(input.errors?.minlength){
+        return `${fieldName} must be at least ${input.errors.minlength.requiredLength} characters.`
+      }
+      return null;
+  }
+
+  matchPasswords(form:NgForm):boolean{
+    const password = form.value.password;
+    const repass = form.value.repass;
+
+    return password === repass;
+  }
+
+
   formSubmit(form: NgForm) {
+    if (form.invalid) {
+      return;
+    }
     console.log('Form submitted');
     console.log(form.value);
 
