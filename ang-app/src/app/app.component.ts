44bfa68124408d5919c76abf06da45a6a6f5f1d9
DianaAsli <dianaasli201001@gmail.com>
2024-12-10 22:15:27 +0200

    Add routerLink to navbar, register, login component

--- ang-app/src/app/app.component.ts
+++ ang-app/src/app/app.component.ts
@@ -3,14 +3,12 @@ import { HeaderComponent } from './layouts/header/header.component';
 import { NavbarComponent } from './layouts/navbar/navbar.component';
 import { FooterComponent } from './layouts/footer/footer.component';
 import { RouterOutlet } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, HeaderComponent, NavbarComponent, FooterComponent],
-  //providers:[HttpClient],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
